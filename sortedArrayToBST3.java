a0a3bc2e6565f86e4b62268f65fb71a87333795b
MirageM <imiragesz@gmail.com>
2023-10-29 15:42:11 -0400

    int mid = left + (right - left)/2;

--- sortedArrayToBST3.java
+++ sortedArrayToBST3.java
@@ -17,14 +17,14 @@ class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
         return sortedArrayToBST(nums, 0, nums.length - 1);
     }
-    private TreeNode sortedArrayToBST(int[] nums, int l, int r){
-        if(l > r){
+    private TreeNode sortedArrayToBST(int[] nums, int left, int right){
+        if(left > right){
             return null;
         }
-        int mid = l + (r - l)/2;
+        int mid = left + (right - left)/2;
         TreeNode root = new TreeNode(nums[mid]);
-        root.left = sortedArrayToBST(nums, l, mid - 1);
-        root.right = sortedArrayToBST(nums, mid + 1, r);
+        root.left = sortedArrayToBST(nums, left, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, right);
         return root;
     }
 }
